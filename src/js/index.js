f0509fa540fabe9df4ffb131137021abf901b8aa
Bryce St. Pierre <brycelstpierre@gmail.com>
2021-05-21 16:52:13 -0400

    Added base styling.

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,31 @@
-import 'normalize.css';
-import '../css/styles.css';
+import "normalize.css";
+import "../css/layout.css";
+import "../css/core.css";
 
-import $ from 'jquery';
+import $ from "jquery";
+
+$(function () {
+  $("#back").on("click", function () {
+    if ($("#stepThree").hasClass("active")) {
+      $("section").removeClass("active");
+      $("#stepTwo").addClass("active");
+      $("#next").removeAttr("disabled");
+    } else if ($("#stepTwo").hasClass("active")) {
+      $("section").removeClass("active");
+      $("#stepOne").addClass("active");
+      $("#back").attr("disabled", true);
+    }
+  });
+
+  $("#next").on("click", function () {
+    if ($("#stepOne").hasClass("active")) {
+      $("section").removeClass("active");
+      $("#stepTwo").addClass("active");
+      $("#back").removeAttr("disabled");
+    } else if ($("#stepTwo").hasClass("active")) {
+      $("section").removeClass("active");
+      $("#stepThree").addClass("active");
+      $("#next").attr("disabled", true);
+    }
+  });
+})
