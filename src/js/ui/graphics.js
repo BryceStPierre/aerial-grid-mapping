b95a730542a3e89c7cccd5a12b3ca24efda083d6
Bryce St. Pierre <brycelstpierre@gmail.com>
2021-06-08 23:26:37 -0400

    Added method of initializing grid.

--- src/js/ui/graphics.js
+++ src/js/ui/graphics.js
@@ -1,10 +1,13 @@
+import $ from "jquery";
+
 import Point from "../geometry/Point";
+import Line from "../geometry/Line";
 import Polygon from "../geometry/Polygon";
 import Rectangle from "../geometry/Rectangle";
+import Grid from "../geometry/Grid";
 import GraphicsManager from "../utils/GraphicsManager";
 import { createPolygon, createMask, createRectangle } from "../utils/svg";
 import { store, retrieve } from "../utils/localStorage";
-import $ from "jquery";
 
 /**
  * @param {number} width
@@ -41,10 +44,10 @@ export const addStepThreeGraphics = (width, height, svgSelector) => {
   maskRect = createRectangle(maskRect.asGraphic(height));
   maskRect.attr("fill", "#fff");
 
-  let maskPolygon = new Polygon(
+  let constraint = new Polygon(
     retrieve("constraint")._points.map((p) => new Point(p._x, p._y))
   );
-  maskPolygon = createPolygon(maskPolygon.asGraphic(height), false);
+  let maskPolygon = createPolygon(constraint.asGraphic(height), false);
   maskPolygon.attr("fill", "#000");
 
   // Add mask to trace constraint polygon.
@@ -60,5 +63,32 @@ export const addStepThreeGraphics = (width, height, svgSelector) => {
   overlay.attr("mask", "url(#overlayMask)");
   $(svgSelector).append(overlay);
 
-  //let manager = new GraphicsManager(width, height, svgSelector);
+  // Compute slope for the initial line used for the grid.
+  const slopes = constraint.segments
+    .filter((line) => line.slope > 0)
+    .map((line) => {
+      if (!isFinite(line.slope)) {
+        if (line.slope == Number.POSITIVE_INFINITY) return 500;
+        if (line.slope == Number.NEGATIVE_INFINITY) return -500;
+      }
+      return line.slope;
+    });
+  const averageSlope = slopes.reduce((a, c) => a + c) / slopes.length;
+
+  let pointWithMinimumX = new Point(
+    constraint.points[0].x + (Math.random() > 0.5 ? 0.001 : -0.001),
+    constraint.points[0].y
+  );
+  constraint.points.forEach((p) => {
+    if (p.x < pointWithMinimumX.x) pointWithMinimumX = p;
+  });
+
+  let manager = new GraphicsManager(width, height, svgSelector);
+  // let grid = new Grid(
+  //   width,
+  //   height,
+  //   20,
+  //   new Line(averageSlope, pointWithMinimumX)
+  // );
+  // manager.addGraphic(grid);
 };
